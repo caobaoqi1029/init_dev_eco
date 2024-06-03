e3b2893aebeadb253d9c4eaa90b7c41103045d7e
曹宝琪 <2024cbq@gmail.com>
2024-06-03 17:21:14 +0800

    fix: fix docs style

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/init_dev_eco/',
   title: "Harmony OS Study",
   description: "鸿蒙 启动！",
 
